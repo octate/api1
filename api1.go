9b0124145d4f42455988bd84898d4113aa7a9adb
Prince Raj <prince.raj@freshvnf.com>
2022-06-21 18:12:12 +0530

    wip task

--- api1.go
+++ api1.go
@@ -1,170 +1,129 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Movie struct {
-	Name           string
-	Genre          string
-	LeadStudio     string
-	AudienceScore  string
-	Profitability  string
-	RottenTomatoes string
-	WorldwideGross string
-	Year           string
+type User struct {
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Phone  string `json:"phone"`
+	UserID string `json:"user_id"`
+	City   string `json:"city"`
 }
 
-var Data []Movie
+var Data map[string]User
 
-func populateData() {
-	records := readCsvFile("./movies.csv")
-	for i := 1; i < len(records); i++ {
-		movie := Movie{
-			Name:           records[i][0],
-			Genre:          records[i][1],
-			LeadStudio:     records[i][2],
-			AudienceScore:  records[i][3],
-			Profitability:  records[i][4],
-			RottenTomatoes: records[i][5],
-			WorldwideGross: records[i][6],
-			Year:           records[i][7],
-		}
-		Data = append(Data, movie)
-	}
-}
 func main() {
-	populateData()
+	Data = make(map[string]User)
 	r := gin.Default()
 	setupRoutes(r)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func setupRoutes(r *gin.Engine) {
-	r.GET("/year/:year", Dummy)
-	r.GET("/rating/:rating", Dummy1)
-	r.GET("/genre/:genre", Dummy2)
+	r.GET("/user/:user_id", GetUserByUserID)
+	r.GET("/user", GetAllUser)
+	r.POST("/user", CreateUser)
+	r.PUT("/user/:user_id", UpdateUser)
+	r.DELETE("/user/:user_id")
 }
 
-//Dummy function
-func Dummy(c *gin.Context) {
-
+//GetUserByUserID function
+func GetUserByUserID(c *gin.Context) {
 	//records := readCsvFile("./movies.csv")
-	year, ok := c.Params.Get("year")
-	genre, ok := c.GetQuery("genre")
-	movieNames := getYear(year, genre)
+	userID, ok := c.Params.Get("user_id")
 	if ok == false {
 		res := gin.H{
-			"error": "name is missing",
+			"error": "user_id is missing",
 		}
 		c.JSON(http.StatusOK, res)
 		return
 	}
-
+	user := getUserByID(userID)
 	res := gin.H{
-		"year":   year,
-		"movies": movieNames,
-		"count":  len(movieNames),
+		"user": user,
 	}
 	c.JSON(http.StatusOK, res)
 }
 
-//Dummy function
-func Dummy1(c *gin.Context) {
-
-	records := readCsvFile("./movies.csv")
-	rating, ok := c.Params.Get("rating")
-	moviesName := getRating(records, rating)
-	if ok == false {
-		res := gin.H{
-			"error": "name is missing",
-		}
-		c.JSON(http.StatusOK, res)
-		return
-	}
-
+//GetAllUser function
+func GetAllUser(c *gin.Context) {
 	res := gin.H{
-		"Name":   moviesName,
-		"Rating": rating,
+		"user": Data,
 	}
 	c.JSON(http.StatusOK, res)
 }
 
-//Dummy function
-func Dummy2(c *gin.Context) {
-
-	records := readCsvFile("./movies.csv")
-	genre, ok := c.Params.Get("genre")
-	moviesName := getGenre(records, genre)
-	if ok == false {
-		res := gin.H{
-			"error": "name is missing",
-		}
-		c.JSON(http.StatusOK, res)
-		return
-	}
-
-	res := gin.H{
-		"Name":  moviesName,
-		"Genre": genre,
-	}
-	c.JSON(http.StatusOK, res)
-}
-
-func readCsvFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
+//CreateUser POST
+func CreateUser(c *gin.Context) {
+	reqBody := User{}
+	err := c.Bind(&reqBody)
 	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
+		res := gin.H{
+			"error": err,
+		}
+		c.JSON(http.StatusBadRequest, res)
+		return
 	}
-	defer f.Close()
+	if reqBody.UserID == "" {
+		res := gin.H{
+			"error": "UserId must not be empty",
+		}
+		c.JSON(http.StatusBadRequest, res)
+		return
+	}
+	Data[reqBody.UserID] = reqBody
+	res := gin.H{
+		"success": true,
+		"user":    reqBody,
+	}
+	c.JSON(http.StatusOK, res)
+	return
+}
 
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
+//Update User PUT
+func UpdateUser(c *gin.Context) {
+	userID, ok := c.Params.Get("user_id")
+	if ok == false {
+		res := gin.H{
+			"error": "user_id is missing",
+		}
+		c.JSON(http.StatusOK, res)
+		return
+	}
+
+	reqBody := User{}
+
+	err := c.Bind(&reqBody)
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
-	}
-
-	return records
-}
-
-func getYear(year, genre string) []Movie {
-	movies := []Movie{}
-	for i := 0; i < len(Data); i++ {
-		if Data[i].Year == year {
-			if genre != "" {
-				if Data[i].Genre == genre {
-					movies = append(movies, Data[i])
-				}
-			} else {
-				movies = append(movies, Data[i])
-			}
+		res := gin.H{
+			"error": err,
 		}
+		c.JSON(http.StatusBadRequest, res)
+		return
 	}
-	return movies
-}
-func getRating(records [][]string, rating string) []string {
-	movieName := []string{}
-	for i := 0; i < len(records); i++ {
-		if records[i][5] >= rating {
-			movieName = append(movieName, records[i][0])
-
+	if reqBody.UserID == "" {
+		res := gin.H{
+			"error": "UserId must not be empty",
 		}
-
+		c.JSON(http.StatusBadRequest, res)
+		return
 	}
-	return movieName
-}
-func getGenre(records [][]string, genre string) []string {
-	movieName := []string{}
-	for i := 0; i < len(records); i++ {
-		if records[i][1] == genre {
-			movieName = append(movieName, records[i][0])
+	if reqBody.UserID != userID {
+		res := gin.H{
+			"error": "UserId cannot be updated",
 		}
+		c.JSON(http.StatusBadRequest, res)
+		return
 	}
-	return movieName
-
+	Data[userID] = reqBody
+	res := gin.H{
+		"success": true,
+		"user":    reqBody,
+	}
+	c.JSON(http.StatusOK, res)
+	return
 }
